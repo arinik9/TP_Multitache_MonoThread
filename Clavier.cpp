eeab2fa19b463ad7f7bb2efdbd7700559fd91b80
arinik9 <nejat@nejat-Inspiron-N5010.(none)>
2014-02-26 18:54:39 +0100

    Version03CommandeQ

--- Clavier.cpp
+++ Clavier.cpp
@@ -1,22 +1,17 @@
 /*************************************************************************
-                           XXX  -  description
+                           Clavier  -  description
                              -------------------
     début                : XXX
     copyright            : (C) XXX par XXX
     e-mail               : XXX
 *************************************************************************/
-//---------- Réalisation de la tâche <Mere> (fichier Mere.cpp) ---
+//---------- Réalisation de la tâche <Clavier> (fichier Clavier.cpp) ---
 /////////////////////////////////////////////////////////////////  INCLUDE
 //-------------------------------------------------------- Include système
-#include <unistd.h>
-#include <Outils.h>
-#include <Heure.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdio.h>
+#include <Menu.h>
+#include <stdlib.h>
+
 //------------------------------------------------------ Include personnel
-#include "Mere.h"
 #include "Clavier.h"
 ///////////////////////////////////////////////////////////////////  PRIVE
 //------------------------------------------------------------- Constantes
@@ -34,26 +29,18 @@
 //} //----- fin de nom
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
-int main()
+void Clavier ()
 // Algorithme :
 //
 {
-	InitialiserApplication(XTERM);
-	pid_t pidHeure = ActiverHeure();
-	pid_t pidClavier = fork();
-	if (pidClavier == -1){
-		perror("fork");
-		exit(EXIT_FAILURE);
+	for(;;) {
+		Menu();
 	}
-	if (pidClavier == 0){
-		Clavier();
-	}
-	else{
-		//Synchronisation de fin normale entre le père et le fils
-		waitpid(pidClavier, NULL, 0);
-	}	
-	kill(pidHeure, SIGUSR2);
-	TerminerApplication(true);
-	return 0;
-} //----- fin de main
+} //----- fin de Clavier
 
+void Commande ( char code, unsigned int valeur ){
+	switch (code) {
+		case 'Q' : exit(0);
+		//case 'P' : 
+	}
+} //----- fin de Commande
