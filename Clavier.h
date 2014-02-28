953cc1274f09884f31d287aa419bd2f58c10847e
Nejat <arinik9@hotmail.com>
2014-02-28 12:26:19 +0100

    Guide de Style

--- Clavier.h
+++ Clavier.h
@@ -1,9 +1,9 @@
 /*************************************************************************
-                           Clavier  -  description
+                           Clavier.h  -  description
                              -------------------
-    début                : XXX
-    copyright            : (C) XXX par XXX
-    e-mail               : XXX
+    début                : Mardi 21 Février 2014  
+    copyright            : (C) 2014 par B3329
+    e-mail               : william.vitali@insa-lyon.fr; nejat.arinik@insa-lyon.fr; niamh.lawlor@insa-lyon.fr
 *************************************************************************/
 
 //---------- Interface de la tâche <Clavier> (fichier Clavier.h) -------
@@ -34,4 +34,3 @@ void Clavier();
 void Commande ( char code, unsigned int valeur );
 
 #endif // Clavier_H
-
