323cb58a1e8d08e3558107b0726730463d31725a
Nejat <arinik9@hotmail.com>
2014-02-28 12:28:49 +0100

    Version06 LectureEcriture Dans La Memoire Partagée

--- Mere.h
+++ Mere.h
@@ -1,9 +1,9 @@
 /*************************************************************************
-                           XXX  -  description
+                           Mere.h  -  description
                              -------------------
-    début                : XXX
-    copyright            : (C) XXX par XXX
-    e-mail               : XXX
+    début                : Mardi 21 Février 2014  
+    copyright            : (C) 2014 par B3329
+    e-mail               : william.vitali@insa-lyon.fr; nejat.arinik@insa-lyon.fr; niamh.lawlor@insa-lyon.fr
 *************************************************************************/
 
 //---------- Interface de la tâche <Mere> (fichier Mere.h) -------
@@ -23,6 +23,8 @@
 
 //------------------------------------------------------------------ Types
 //////////////////////////////////////////////////////////////////  PUBLIC
+int shmid ;    // l'identificateur du segment de memoire partagee
+
 //---------------------------------------------------- Fonctions publiques
 // type Nom ( liste de paramètres );
 // Mode d'emploi :
@@ -32,4 +34,3 @@
 int main();
 
 #endif // Mere_H
-
